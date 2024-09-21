04525f2aa8abdac8be7b4319de3235fdf9043e65
po3nx <ipo3nx@gmail.com>
2024-09-21 13:33:01 +0800

    Add Indonesian Flag

---
+++ flag.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+r=9
+w=15
+
+printf "\e[48;5;${r}m      \e[0m\e[48;5;${r}m \e[0m INDONESIA \e[0m\e[48;5;${r}m       \e[0m\n"
+printf "\e[48;5;${w}m      \e[0m\e[48;5;${w}m \e[0m           \e[0m\e[48;5;${w}m       \e[0m\n"
